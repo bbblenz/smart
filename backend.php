2cd352c181b886efd03805c2b6f268a0d5dc75cf
Arpit Raj <188745500+bbblenz@users.noreply.github.com>
2025-03-08 21:56:21 +0530

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,17 +1,27 @@
 <?php
-    error_reporting(0);
-    if($_POST['register'])
-        {
+    if (isset($_POST['register'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $query = "INSERT INTO mytable VALUES('$username','$password')";
-        $data = mysqli_query($checker,$query);
-        if($data){
-            // echo "inserted into database";
-        }
-        else{
-            echo "failed to insert";
+        // Validate user input
+        if (!empty($username) && !empty($password)) {
+
+            // Hash the password
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Use prepared statements
+            $query = "INSERT INTO mytable (username, password) VALUES ($1, $2)";
+            $result = pg_query_params($checker, $query, array($username, $hashed_password));
+
+            // Execute the query
+            if ($result) {
+                echo "Inserted into database";
+            } else {
+                echo "Failed to insert";
+            }
+
+        } else {
+            echo "Username and password cannot be empty";
         }
     }
 ?>
