405d8aec43826f276ecfb9eca0e7d4b88b73c530
Arpit Raj <188745500+bbblenz@users.noreply.github.com>
2025-03-08 21:49:45 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-   DATABASE_URL=postgresql://postgres:@db.wsemrqsksekxrrowzeuh.supabase.co:5432/postgres
+   DATABASE_URL=postgresqli://postgres:@db.wsemrqsksekxrrowzeuh.supabase.co:5432/postgres
 
     //connect
     $checker = mysqli_connect($postgres,$db.wsemrqsksekxrrowzeuh.supabase.co,$password,$postgres);
