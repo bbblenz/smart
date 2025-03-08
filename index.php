c22d2f29bad32204e4a0e0eaccf7411df734c1d0
Arpit Raj <188745500+bbblenz@users.noreply.github.com>
2025-03-08 22:26:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ error_reporting(0);
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page | attendance</title>
+    <title>Login Page | Attendance</title>
 </head>
 <body>
     <div class="login-container">
-        <h2>Welcome !</h2>
+        <h2>Welcome!</h2>
         <form action="#" method="POST">
             <input type="text" placeholder="Username" name="username">
             <input type="password" placeholder="Password" name="password">
@@ -23,6 +23,4 @@ error_reporting(0);
     </div>
 </body>
 </html>
-
-   
 
